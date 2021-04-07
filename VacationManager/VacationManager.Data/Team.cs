bc86ee04c5260c49096843468073b3041c176fba
fraxiq <alexns007@abv.bg>
2021-04-07 18:43:05 +0300

    code

---
+++ VacationManager/VacationManager.Data/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VacationManager.Data
+{
+    public class Team 
+    {
+        public Team()
+        {
+            this.Users = new HashSet<ApplicationUser>();
+            this.Projects = new HashSet<Project>();
+        }
+
+        [Key]
+        public int ID { get; set; }
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+        public string TeamName { get; set; }
+        
+
+
+    }
+}
---
+++ VacationManager/VacationManager.Web/Startup.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VacationManager.Data;
+using VacationManager.Data.Data;
+
+namespace VacationManager.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<VacationDbContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+            services.AddControllersWithViews();
+            services.AddIdentityCore<ApplicationUser>()
+        .AddEntityFrameworkStores<VacationDbContext>();
+
+            services.BuildServiceProvider().GetService<VacationDbContext>().Database.EnsureCreated();
+            var context = services.BuildServiceProvider().GetService<VacationDbContext>();
+
+            string[] roles = new string[] { "CEO", "Developer", "Team Lead", "Unassigned" };
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+
+            //if (!context.Roles.Any(r => r.Name == "CEO"))
+            //{
+            //    var store = new RoleStore<IdentityRole>(context);
+            //    var manager = new RoleManager<IdentityRole>(store);
+            //    var adminRole = new IdentityRole { Name = "Admin" };
+
+            //    manager.Create(adminRole);
+            //    roleStore.CreateAsync(new IdentityRole("Unassigned"));
+            //    roleStore.CreateAsync(new IdentityRole("CEO"));
+            //    roleStore.CreateAsync(new IdentityRole("Developer"));
+            //    roleStore.CreateAsync(new IdentityRole("Team Lead"));         
+            //}
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
